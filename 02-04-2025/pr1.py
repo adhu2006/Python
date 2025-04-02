8c988fcd917f6a8bb831c02d1135d329f46b2375
adhu2006 <sbalamuruganadarshbca24@rvu.edu.in>
2025-04-02 15:08:35 +0530

    Add files via upload

---
+++ 02-04-2025/pr1.py
@@ -0,0 +1,20 @@
+def calculator():
+    print("simple calculator")
+    num1=float(input("enter first number:"))
+    num2=float(input("enter second number:"))
+    print("select operation:")
+    print("1.add")
+    print("2.divide")
+    choice=input("enter choice(1/2):")
+    if choice=='1':
+      print(f"result:{num1}+{num2}={num1+num2}")
+    elif choice=='2':
+      if num2!=0:
+        print(f"result:{num1}/{num2}={num1/num2}")
+      else:
+        print("error!division by zero.")
+    else: 
+        print("invalid input")
+calculator()
+      
+      
